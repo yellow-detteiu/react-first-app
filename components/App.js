099ee767f48e8c6c1fae2950ee5dd5e8eee1784a
中野　翔太 <nakanoshota@nakanoshoutanoMacBook-Air.local>
2022-06-04 08:47:07 +0900

    my first change

---
+++ components/App.js
@@ -0,0 +1,23 @@
+import React, { useReducer } from 'react'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import EventForm from './EventForm'
+import Events from './Events'
+import AppContext from '../contexts/AppContext'
+import reducer from '../reducers'
+
+const App = () => {
+    const [state, dispatch] = useReducer(reducer, [])
+
+    return (
+        <AppContext.Provider value={{ state, dispatch }}>
+            <div className="container-fruid">
+                <EventForm />
+                <Events />
+            </div>
+        </AppContext.Provider>
+    )
+}
+
+export default App
